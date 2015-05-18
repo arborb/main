df2d6419d6e893ccac6efbe70a0ea271001299f2
sjjeon <dbfire2@nate.com>
2015-05-18 20:33:13 +0900

    #서버 누락소스

---
+++ java/nexos/service/cm/CM20010EDAO.java
@@ -0,0 +1,15 @@
+package nexos.service.cm;
+
+import java.util.Map;
+
+public interface CM20010EDAO {
+
+  /**
+   * 용기마스터 내역 조회
+   * @param params
+   * @return
+   * @throws Exception
+   */
+  void save(Map<String, Object> params) throws Exception;
+
+}
---
+++ java/nexos/service/cm/CM20010EDAOImpl.java
@@ -0,0 +1,68 @@
+package nexos.service.cm;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import nexos.common.Consts;
+import nexos.common.ibatis.NexosDAO;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Class: CM20010EDAOImpl<br>
+ * Description: CM20010E DAO (Data Access Object)<br>
+ * Copyright: Copyright (c) 2013 ASETEC Corporation. All rights reserved.<br>
+ * Company : ASETEC<br>
+ * 
+ * @author ASETEC
+ * @version 1.0
+ * 
+ * <pre style="font-family: NanumGothicCoding, GulimChe">
+ * ---------------------------------------------------------------------------------------------------------------------
+ *  Version    Date          Author           Description
+ * ---------  ------------  ---------------  ---------------------------------------------------------------------------
+ *  1.0        2013-01-01    ASETEC           신규작성
+ * ---------------------------------------------------------------------------------------------------------------------
+ * </pre>
+ */
+@Repository("CM20010EDAO")
+public class CM20010EDAOImpl implements CM20010EDAO {
+
+  // private final Logger logger = LoggerFactory.getLogger(CM20010EDAOImpl.class);
+
+  @Resource
+  private NexosDAO nexosDAO;
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public void save(Map<String, Object> params) throws Exception {
+
+    List<Map<String, Object>> saveMasterDS = (List<Map<String, Object>>)params.get(Consts.PK_DS_MASTER);
+    String user_Id = (String)params.get(Consts.PK_USER_ID);
+
+    final String PRORAM_ID = "CM20010E";
+    final String TABLE_NM = "CMPICKBOX";
+    final String INSERT_ID = PRORAM_ID + ".INSERT_" + TABLE_NM;
+    final String UPDATE_ID = PRORAM_ID + ".UPDATE_" + TABLE_NM;
+    final String DELETE_ID = PRORAM_ID + ".DELETE_" + TABLE_NM;
+
+    // DataSet 처리
+    int dsCnt = saveMasterDS.size();
+    for (int i = 0; i < dsCnt; i++) {
+      Map<String, Object> rowData = saveMasterDS.get(i);
+      rowData.put(Consts.PK_REG_USER_ID, user_Id);
+
+      String crud = (String)rowData.get(Consts.PK_CRUD);
+      if (Consts.DV_CRUD_C.equals(crud)) {
+        nexosDAO.insert(INSERT_ID, rowData);
+      } else if (Consts.DV_CRUD_U.equals(crud)) {
+        nexosDAO.update(UPDATE_ID, rowData);
+      } else if (Consts.DV_CRUD_D.equals(crud)) {
+        nexosDAO.delete(DELETE_ID, rowData);
+      }
+    }
+  }
+
+}
